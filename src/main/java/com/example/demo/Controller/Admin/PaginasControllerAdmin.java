5f33c3197839b477cf2ee215276afe0a89039c2a
AlssnQu <alifquiaqz@gmail.com>
2025-11-12 22:02:49 -0500

    Arreglos en PagController

---
+++ src/main/java/com/example/demo/Controller/Admin/PaginasControllerAdmin.java
@@ -0,0 +1,66 @@
+package com.example.demo.Controller.Admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PaginasControllerAdmin {
+
+    //Métricas
+    @GetMapping("/ver-metricasInicio")
+    public String mostrarMetricasInicio() {
+        return "admin/Metricas/metricas";
+    }
+
+    @GetMapping("/ver-metricaEvSuscripciones")
+    public String mostrarMetricaEvSuscripciones() {
+        return "admin/Metricas/metricaEvolucionSuscripciones";
+    }
+
+    @GetMapping("/ver-metricaMetPago")
+    public String mostrarMetricaMetPago() {
+        return "admin/Metricas/metricaMetodosPago";
+    }
+
+    @GetMapping("/ver-metricaTasaRenov")
+    public String mostrarMetricaTasaRenov() {
+        return "admin/Metricas/metricaTasaRenovacion";
+    }
+
+    //CRUDs
+    @GetMapping("/ver-CRUDsInicio")
+    public String mostrarCRUDsInicio() {
+        return "admin/CRUDs/CRUDs";
+    }
+
+    @GetMapping("/ver-CRUDPlanes")
+    public String mostrarCRUDPlanes() {
+        return "admin/CRUDs/planesCRUD";
+    }
+
+    @GetMapping("/ver-CRUDMembresias")
+    public String mostrarCRUDMembresias() {
+        return "admin/CRUDs/membresiasCRUD";
+    }
+
+    @GetMapping("/ver-CRUDTipoProducto")
+    public String mostrarCRUDTipoProducto() {
+        return "admin/CRUDs/TipoProductoCRUD";
+    }
+
+    @GetMapping("/ver-CRUDProductos")
+    public String mostrarCRUDProductos() {
+        return "admin/CRUDs/productosCRUD";
+    }
+
+    @GetMapping("/ver-CRUDUsuarios")
+    public String mostrarCRUDUsuarios() {
+        return "admin/CRUDs/usuariosCRUD";
+    }
+
+    //Gestión
+    @GetMapping("/ver-gestionInicio")
+    public String mostrarGestionInicio() {
+        return "admin/Gestion/gestionInicio";
+    }
+}
---
+++ src/main/java/com/example/demo/Controller/Cliente/PaginasControllerCliente.java
@@ -0,0 +1,43 @@
+package com.example.demo.Controller.Cliente;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PaginasControllerCliente {
+
+    @GetMapping("/ver-inicio")
+    public String mostrarInicio() {
+        return "usuario/inicio";
+    }
+
+    @GetMapping("/ver-anuncio")
+    public String mostrarAnuncio() {
+        return "usuario/anuncio";
+    }
+
+    @GetMapping("/ver-contacto")
+    public String mostrarContacto() {
+        return "usuario/contacto";
+    }
+
+    @GetMapping("/ver-login")
+    public String mostrarLogin() {
+        return "login/login";
+    }
+
+    @GetMapping("/ver-membresias")
+    public String mostrarMembresias() {
+        return "usuario/membresias";
+    }
+
+    @GetMapping("/ver-pagos")
+    public String mostrarPagos() {
+        return "usuario/pagos";
+    }
+
+    @GetMapping("/ver-productos")
+    public String mostrarProductos() {
+        return "usuario/productos";
+    }
+}
